f3b8130464e9f0362d7fb581586c2d3076c77849
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-29 22:25:38 +0900

    1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {  
+       string[] s = ("c","h","a","o");
+       Console.WriteLine("Nhap ky tu tu ban phim");
+       string inputS = Console.ReadLine();
+       Console.WriteLine(inputS);
+       int count=0;
+       for (int i = 0; i < s.Length; i++)
+       {
+        if(s[1]==inputS)
+        {
+            count+=1;
+        }
+       }
+       Console.WriteLine("So ky tu co trong chuoi la"+ count);
+    }
+}
